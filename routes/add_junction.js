aa8486212cbddb326d9923b3f55938401c4da43e
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-12-09 20:18:46 +0530

    Minor changes to project structure.

--- routes/add_junction.js
+++ routes/add_junction.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
@@ -8,14 +7,3 @@ router.get('/junction', function(req, res, next) {
 });
 
 module.exports = router;
-=======
-var express = require('express');
-var router = express.Router();
-
-/* GET add_Junction page. */
-router.get('/junction', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
->>>>>>> 67a00c80c60d9da0fe2b68cf924b17b431dd892d
