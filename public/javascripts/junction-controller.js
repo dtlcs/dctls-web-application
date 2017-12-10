f580a2322c22f9551f96faaf2097b8f93a33fba3
sachini93 <sachini.thaksala@gmail.com>
2017-12-10 05:58:19 -0800

    add Junction completed

--- public/javascripts/junction-controller.js
+++ public/javascripts/junction-controller.js
@@ -3,10 +3,24 @@
  */
 
 "use strict";
-app.controller('App-controller-junction',function ($scope,$http) {
+// console.log('aaa');
+app.controller('App-controller-junction',function ($scope,$http,$cookieStore) {
+    console.log('qwe');
+    // $scope.newJunction={
+    //     name:'ww',
+    //     street:'s',
+    //     city:'2e',
+    //     province:'ww',
+    //     postal_code:'22',
+    //     latitude:'2233.4',
+    //     longitude:'334.99',
+    //     pi_mac:'11s',
+    //
+    // };
 
     //Add new Junction
-    $scope.addJunction=function () {
+    $scope.add=function () {
+        console.log('pppp');
         console.log($scope.newJunction);
         $http.post('http://localhost:8080/api/junction/add', $scope.newJunction).then(function (res, err) {
             console.log(res);
