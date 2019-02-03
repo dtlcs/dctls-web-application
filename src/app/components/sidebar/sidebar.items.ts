ef017ff91927b672822141b13c7dd77e5cdbe232
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-02-03 11:17:20 +0530

    Init project

---
+++ src/app/components/sidebar/sidebar.items.ts
@@ -0,0 +1,34 @@
+export const SIDEBAR_ITEMS = [
+    {
+        label: 'TMS Now',
+        icon: 'pi pi-pw pi-file',
+        items: []
+    },
+    {
+        label: 'Payments',
+        icon: 'pi pi-pw pi-file',
+        items: []
+    },
+    {
+        label: 'Students',
+        icon: 'pi pi-pw pi-file',
+        items: []
+    },
+    {
+        label: 'Announcements',
+        icon: 'pi pi-pw pi-file',
+        items: []
+    },
+    {
+        label: 'Inbox',
+        icon: 'pi pi-pw pi-file',
+    },
+    {
+        label: 'Calendar',
+        icon: 'pi pi-pw pi-file',
+    },
+    {
+        label: 'Settings',
+        icon: 'pi pi-pw pi-file',
+    }
+];
